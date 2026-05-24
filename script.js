c4a60d1b557a8be48f89af2cc2b7380bca0c0f38
Milenna Alves <milennafeijocanal@gmail.com>
2026-05-23 22:22:10 -0300

    feat: criando elementos separadamente para renderizar na tela, e colocando uma lixeira ao lado do botao de delete

--- script.js
+++ script.js
@@ -3,30 +3,40 @@ const textInput = document.getElementById('text-input');
 const todoList = document.getElementById('todo-list');
 
 
-buttonAdd.addEventListener('click', function() {
+buttonAdd.addEventListener('click', function () {
     const text = textInput.value.trim();
     if (!text) return;
 
     const item = document.createElement('label');
     item.classList.add('todo-item');
-    item.innerHTML = `
-        <input type="checkbox" class="checkbox">
-        <span class="task-text">${text}</span>
-        <button class="delete-button">Excluir</button>
-    `;
 
-    const deleteButton = item.querySelector('.delete-button');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('checkbox');
 
-    deleteButton.addEventListener ('click', function () {
+    const taskText = document.createElement('span');
+    taskText.classList.add('task-text');
+    taskText.textContent = text;
+
+    const button = document.createElement('button');
+    button.classList.add('delete-button');
+    button.innerHTML = '🗑 &nbsp; Excluir';
+
+    item.appendChild(checkbox);
+    item.appendChild(taskText);
+    item.appendChild(button);
+
+    button.addEventListener('click', function () {
         todoList.removeChild(item);
     });
+
 
     todoList.appendChild(item);
     textInput.value = '';
     textInput.focus();
 });
 
-textInput.addEventListener('keydown', function(event) {
+textInput.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
         buttonAdd.click();
     }
