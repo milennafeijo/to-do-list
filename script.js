caccfbd0fa6e49b2d80d911ab1618a4f61f4760f
Milenna Alves <milennafeijocanal@gmail.com>
2026-05-24 13:29:55 -0300

    feat: adiciona tarefas adicionads e excluidas no localStorage, fix: corrige array tarefas nao sendo mantido ao carregar a pagina

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const tarefas = []
+// array para armazenar as tarefas
 
+const tarefas = [];
+
+// variáveis para acessar os elementos do DOM
 
 const buttonAdd = document.getElementById('add-button');
 const textInput = document.getElementById('text-input');
 const todoList = document.getElementById('todo-list');
 
+// função para criar um item da lista de tarefas
 
-buttonAdd.addEventListener('click', function () {
-    const text = textInput.value.trim();
-    if (!text) return;
+function criarItem(text) {
 
     const item = document.createElement('label');
     item.classList.add('todo-item');
@@ -25,19 +27,51 @@ buttonAdd.addEventListener('click', function () {
     button.classList.add('delete-button');
     button.innerHTML = '🗑 &nbsp; Excluir';
 
+    // Adiciona os elementos ao item da lista
+
     item.appendChild(checkbox);
     item.appendChild(taskText);
     item.appendChild(button);
 
+    // Adiciona o evento de clique ao botão de excluir
+
     button.addEventListener('click', function () {
         todoList.removeChild(item);
+        tarefas.splice(tarefas.indexOf(text), 1);
+        localStorage.setItem('tarefas', JSON.stringify(tarefas));
     });
 
+    // Adiciona o item à lista de tarefas
 
     todoList.appendChild(item);
+
+}
+
+// Evento de clique para o botão de adicionar tarefa
+
+buttonAdd.addEventListener('click', function () {
+    const text = textInput.value.trim();
+    if (!text) return;
+
+    criarItem(text);
+
+    tarefas.push(text);
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+
     textInput.value = '';
     textInput.focus();
+
 });
+
+// Carrega as tarefas salvas no localStorage ao carregar a página
+
+localStorage.getItem('tarefas') && JSON.parse(localStorage.getItem('tarefas')).forEach(function (text) {
+    criarItem(text);
+    tarefas.push(text);
+});
+
+
+// Permite adicionar a tarefa pressionando a tecla Enter
 
 textInput.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
