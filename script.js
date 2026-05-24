d37600c2f9a7f755de146dfbd71c37232bcd28a4
Milenna Alves <milennafeijocanal@gmail.com>
2026-05-24 13:59:39 -0300

    refactor: move font-family ao body e arruma transitions para o lugar certo, troca todoList.removeChild(item) por item.remove()

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-// array para armazenar as tarefas
-
 const tarefas = [];
-
-// variáveis para acessar os elementos do DOM
 
 const buttonAdd = document.getElementById('add-button');
 const textInput = document.getElementById('text-input');
 const todoList = document.getElementById('todo-list');
-
-// função para criar um item da lista de tarefas
 
 function criarItem(text) {
 
@@ -27,27 +21,20 @@ function criarItem(text) {
     button.classList.add('delete-button');
     button.innerHTML = '🗑 &nbsp; Excluir';
 
-    // Adiciona os elementos ao item da lista
-
     item.appendChild(checkbox);
     item.appendChild(taskText);
     item.appendChild(button);
 
-    // Adiciona o evento de clique ao botão de excluir
-
     button.addEventListener('click', function () {
-        todoList.removeChild(item);
+        item.remove();
         tarefas.splice(tarefas.indexOf(text), 1);
         localStorage.setItem('tarefas', JSON.stringify(tarefas));
     });
-
-    // Adiciona o item à lista de tarefas
 
     todoList.appendChild(item);
 
 }
 
-// Evento de clique para o botão de adicionar tarefa
 
 buttonAdd.addEventListener('click', function () {
     const text = textInput.value.trim();
@@ -60,18 +47,14 @@ buttonAdd.addEventListener('click', function () {
 
     textInput.value = '';
     textInput.focus();
-
 });
 
-// Carrega as tarefas salvas no localStorage ao carregar a página
 
 localStorage.getItem('tarefas') && JSON.parse(localStorage.getItem('tarefas')).forEach(function (text) {
     criarItem(text);
     tarefas.push(text);
 });
 
-
-// Permite adicionar a tarefa pressionando a tecla Enter
 
 textInput.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
